17dd69e9c213771a0e540085086a5fc07b6769d1
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2025-08-11 17:21:24 +0900

    db 연동

--- src/bokchi/java/config/ConnectionProvider.java
+++ src/bokchi/java/config/ConnectionProvider.java
@@ -1,22 +1,22 @@
-package bokchi.java.config;
+ package bokchi.java.config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionProvider {
-    private static final String DB_NAME = "stamp_kiosk"; // ← 네 DB명으로 수정 가능
+    private static final String DB_NAME = "stamp_kiosk";
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL 8.x 드라이버
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("드라이버 로드 성공");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("[DB] MySQL Driver not found", e);
         }
     }
 
     public static Connection getConnection() throws SQLException {
-        // MySQLConnInfo.URL == "jdbc:mysql://localhost:3306"
         String url = MySQLConnInfo.URL + "/" + DB_NAME
                    + "?useSSL=false&serverTimezone=UTC&characterEncoding=utf8&allowPublicKeyRetrieval=true";
         return DriverManager.getConnection(url, MySQLConnInfo.USER, MySQLConnInfo.PASSWORD);
--- src/bokchi/java/config/Main.java
+++ src/bokchi/java/config/Main.java
@@ -1,10 +1,16 @@
 package bokchi.java.config;
 
+import bokchi.java.ui.staff.StaffLoginFrame;
+import bokchi.java.ui.user.UserLoginFrame;
+
+import javax.swing.*;
+
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            // 둘 중 하나를 켜서 테스트
+//            new StaffLoginFrame().setVisible(true);
+          new UserLoginFrame().setVisible(true);
+        });
+    }
 }
